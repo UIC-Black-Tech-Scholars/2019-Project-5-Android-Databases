69cac591f8db85b2ad11a72cad45882ff0dbbcf8
lucasoskorep <lucas.oskorep@gmail.com>
2019-02-04 12:34:08 -0600

    Adding in the initial commit of code, also adding in finished code but uncommented.

---
+++ app/src/main/java/com/bts/lucasoskorep/project5androiddatabases/DAO/UserDao.java
@@ -0,0 +1,25 @@
+package com.bts.lucasoskorep.project5androiddatabases.DAO;
+
+import android.arch.persistence.room.Dao;
+
+@Dao
+public interface UserDao {
+
+//    @Query("SELECT * FROM user")
+//    List<User> getAll();
+//
+//    @Query("SELECT * FROM user where first_name LIKE  :firstName AND last_name LIKE :lastName")
+//    User findByName(String firstName, String lastName);
+//
+//    @Query("SELECT COUNT(*) from user")
+//    int countUsers();
+//
+//    @Update
+//    void updateUsers(User... users);
+//
+//    @Insert
+//    void insertAll(User... users);
+//
+//    @Delete
+//    void delete(User user);
+}
